9020f1d2a3dbf7444d00a9ab40c788213a4dcca6
SamCrofts <44583071+SamCrofts@users.noreply.github.com>
2019-01-08 16:57:10 +0000

    Add files via upload

---
+++ Winscreen2.m
@@ -0,0 +1,102 @@
+function varargout = Winscreen2(varargin)
+% WINSCREEN2 MATLAB code for Winscreen2.fig
+%      WINSCREEN2, by itself, creates a new WINSCREEN2 or raises the existing
+%      singleton*.
+%
+%      H = WINSCREEN2 returns the handle to a new WINSCREEN2 or the handle to
+%      the existing singleton*.
+%
+%      WINSCREEN2('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in WINSCREEN2.M with the given input arguments.
+%
+%      WINSCREEN2('Property','Value',...) creates a new WINSCREEN2 or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before Winscreen2_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to Winscreen2_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help Winscreen2
+
+% Last Modified by GUIDE v2.5 08-Jan-2019 16:43:16
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @Winscreen2_OpeningFcn, ...
+                   'gui_OutputFcn',  @Winscreen2_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before Winscreen2 is made visible.
+function Winscreen2_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to Winscreen2 (see VARARGIN)
+
+% Choose default command line output for Winscreen2
+handles.output = hObject;
+
+
+% create an axes that spans the whole gui
+ah = axes('unit', 'normalized', 'position', [0 0 1 1]); 
+% import the background image and show it on the axes
+bg = imread('winscreen.jpg'); imagesc(bg);
+% prevent plotting over the background and turn the axis off
+set(ah,'handlevisibility','off','visible','off')
+% making sure the background is behind all the other uicontrols
+uistack(ah, 'bottom');
+
+
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes Winscreen2 wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = Winscreen2_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in quit.
+function quit_Callback(hObject, eventdata, handles)
+% hObject    handle to quit (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+close all
+
+% --- Executes on button press in startagain.
+function startagain_Callback(hObject, eventdata, handles)
+% hObject    handle to startagain (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+close (Winscreen2)
+
+run Homescreen.m
